93fde67052bf6c9acc25a438bd931723dd0e6ff9
ClaireMarieV <>
2021-02-12 20:59:41 +0100

    changed to grid gp 2rem

--- components/Content.js
+++ components/Content.js
@@ -9,7 +9,7 @@ const Content = ({ children }) => (
         grid-template-columns: repeat(2, minmax(0, 1fr));
         width: 80vw;
         margin: 4rem auto 4rem auto;
-        grid-gap: 3rem;
+        grid-gap: 2rem;
       }
     `}</style>
   </div>

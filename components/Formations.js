5fe8b20f086e29ae41de3a06f50989664e1753a8
ClaireMarieV <clairevan.54.cv@gmail.com>
2023-03-14 13:30:53 +0100

    Changed title formation

--- components/Formations.js
+++ components/Formations.js
@@ -2,7 +2,7 @@ import React from "react";
 import Link from "next/link";
 
 const Experiences = () => (
-  <>
+  <div>
     <div className="img">
       <img src="/svg/pivoineYellow.svg" />
     </div>
